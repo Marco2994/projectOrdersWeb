d7072a8201d6f3402a208472f54a2f179d762bfd
Marco <marco_al94@hotmail.com>
2021-12-05 17:36:14 -0500

    Primer Avance Web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { OrdenCompraComponent } from './components/ordenes/orden-compra/orden-compra.component';
+import { OrdenServicioComponent } from './components/ordenes/orden-servicio/orden-servicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'login',  pathMatch: 'full'},
+  {path:'login', component: LoginComponent },
+  {path:'oServicio', component: OrdenServicioComponent },
+  {path:'oCompra', component: OrdenCompraComponent },
+  {path:'home', component: HomeComponent },
+  {path:'**', redirectTo: 'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { OrdenServicioComponent } from './components/ordenes/orden-servicio/orden-servicio.component';
+import { OrdenCompraComponent } from './components/ordenes/orden-compra/orden-compra.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    OrdenCompraComponent,
+    OrdenServicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
